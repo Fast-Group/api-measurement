f78bba6e50bb6073bf4990baaea83e10626c9fe5
Jason Jan Ngo <jgngo@fast.com.ph>
2024-04-11 17:49:50 +0800

    add initial api

---
+++ server/api/measurements.ts
@@ -0,0 +1,27 @@
+export default defineEventHandler((event) => {
+    const payload = {
+        "Message": "success",
+        "Barcode": "YT2318522147003012",
+        "ChuteCode": [2]
+    }
+
+    return payload
+})
+
+// {
+//       "Barcode": [
+//         "YT2318522147003012"
+//       ],
+//       "MachineCode": "PAXF-01",
+//       "Weight": 0.0,
+//       "Length": 0.0,
+//       "Width": 0.0,
+//       "Height": 0.0,
+//       "RequestTime": "2023-06-08 15:09:29"
+//     }
+
+//     {
+//           "Message": "success",
+//           "Barcode": "YT2318522147003012",
+//           "ChuteCode": [ 2 ]
+//         }
