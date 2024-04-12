b1d6ee1ac90a5e1d874b2c21afa64da27c43f666
Jason Jan Ngo <jgngo@fast.com.ph>
2024-04-12 08:15:22 +0800

    modify index

--- app.vue
+++ app.vue
@@ -1,5 +1,34 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Measurements API</h1>
+    <p>This API receives data from the dimensioning machine.</p>
+    <h2>Request</h2>
+    <p>POST /api/measurements</p>
+    <pre>
+    "Barcode": [
+      "YT2318522147003012"
+      ],
+      "MachineCode": "PAXF-01",
+      "Weight": 0.0,
+      "Length": 0.0,
+      "Width": 0.0,
+      "Height": 0.0,
+      "RequestTime": "2023-06-08 15:09:29"
+    }
+  </pre>
+  <h2>Response</h2>
+  <pre>
+    {
+       "Message": "success",
+       "Barcode": "YT2318522147003012",
+       "ChuteCode": [ 2 ]
+    }
+  </pre>
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Measurements API',
+})
+</script>
