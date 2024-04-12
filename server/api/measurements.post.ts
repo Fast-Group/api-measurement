744453a8e3c710912b5edebe4b5a53ee1c981f6a
Jason Jan Ngo <jgngo@fast.com.ph>
2024-04-12 10:47:38 +0800

    add schema validation

--- server/api/measurements.post.ts
+++ server/api/measurements.post.ts
@@ -12,32 +12,41 @@ const MeasurementSchema = z.object({
 
 export default defineEventHandler(async event => {
     const body = await readBody(event)
-    // const result = MeasurementSchema.safeParse(body)
-    const barcode = body.Barcode[0]
-    console.log(body)
-    const payload = {
-        "Message": "success",
-        "Barcode": barcode,
-        "ChuteCode": [2]
+    const result = MeasurementSchema.safeParse(body)
+    console.log(result)
+    let payload = {}
+
+    if (result.success) {
+        const barcode = result.data.Barcode[0]
+        payload = {
+            "Message": "success",
+            "Barcode": barcode,
+            "ChuteCode": [2]
+        }
+    } else {
+        payload = {
+            "Message": "error",
+            "Error": result.error
+        }
     }
 
     return payload
 })
 
-// // {
-// "Barcode": [
-//     "YT2318522147003012"
-// ],
-//     "MachineCode": "PAXF-01",
-//         "Weight": 0.0,
-//             "Length": 0.0,
-//                 "Width": 0.0,
-//                     "Height": 0.0,
-//                         "RequestTime": "2023-06-08 15:09:29"
-//     }
+// {
+//     "Barcode": [
+//         "YT2318522147003012"
+//     ],
+//         "MachineCode": "PAXF-01",
+//             "Weight": 0.0,
+//                 "Length": 0.0,
+//                     "Width": 0.0,
+//                         "Height": 0.0,
+//                             "RequestTime": "2023-06-08 15:09:29"
+// }
 
-// //     {
-// //           "Message": "success",
-// //           "Barcode": "YT2318522147003012",
-// //           "ChuteCode": [ 2 ]
-// //         }
+// {
+//     "Message": "success",
+//         "Barcode": "YT2318522147003012",
+//             "ChuteCode": [2]
+// }
